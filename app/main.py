b67ba97184466caa7507f6fe8e9f605168aac74f
Rymma Prodan <rymmaprodan6@gmail.com>
2026-07-31 19:15:06 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,22 +1,15 @@
 def get_human_age(cat_age: int, dog_age: int) -> list[int]:
-    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+    if type(cat_age) is not int or type(dog_age) is not int:
         raise TypeError
 
-    def cat_to_human(age: int) -> int:
+    def convert_to_human(age: int, divisor: int) -> int:
         if age < 15:
             return 0
         if age < 24:
             return 1
-        return 2 + (age - 24) // 4
-
-    def dog_to_human(age: int) -> int:
-        if age < 15:
-            return 0
-        if age < 24:
-            return 1
-        return 2 + (age - 24) // 5
+        return 2 + (age - 24) // divisor
 
     return [
-        cat_to_human(cat_age),
-        dog_to_human(dog_age),
+        convert_to_human(cat_age, 4),
+        convert_to_human(dog_age, 5),
     ]
